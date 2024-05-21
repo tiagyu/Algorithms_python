47c1188244737d8cccf0013c420277a905fb4f62
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2024-05-21 11:19:15 +0900

    Add files via upload

---
+++ 자료구조와 함께 배우는 알고리즘 입문/01 알고리즘 기초/01-1 알고리즘이란?/1-5.py
@@ -0,0 +1,12 @@
+#4개로 분기하는 조건문
+#분기에 포함되지 않은 else문이 숨어 있음
+
+n = int(input('정수를 입력하세요:'))
+
+if n==1:
+    print('A')
+elif n==2:
+    print('B')
+elif n==3:
+    print('C')
+    
---
+++ 자료구조와 함께 배우는 알고리즘 입문/01 알고리즘 기초/01-1 알고리즘이란?/1-6.py
@@ -0,0 +1,13 @@
+#실습 1-5의 원래 모습
+#pass문은 '아무것도 수행하지 말고 그냥 지나치세요'를 뜻하는 키워드다
+
+n = int(input('정수를 입력하세요:'))
+
+if n==1:
+    print('A')
+elif n==2:
+    print('B')
+elif n==3:
+    print('C')
+else:
+    pass
