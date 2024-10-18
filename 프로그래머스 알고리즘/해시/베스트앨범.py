0185821fae227132f01273cbd14e277d9836ce85
tiagyu <tkroh1997@gmail.com>
2024-10-18 19:18:11 +0900

    Create 베스트앨범.py

---
+++ 프로그래머스 알고리즘/해시/베스트앨범.py
@@ -0,0 +1,26 @@
+# 베스트앨범
+def solution(genres, plays):
+    
+    answer = []
+    genres_Dict1 = {}
+    genres_Dict2 = {}
+    
+    for i, (g,p) in enumerate(zip(genres, plays)):
+        if g not in genres_Dict1:
+            genres_Dict1[g] = [(i, p)]
+        else:
+            genres_Dict1[g].append((i, p))
+            
+        if g not in genres_Dict2:
+            genres_Dict2[g] = p
+        else:
+            genres_Dict2[g] += p
+    
+    for (k, v) in sorted(genres_Dict2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(genres_Dict1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
+    return answer
+
+print(solution(["classic", "pop", "classic", "classic", "pop"],
+               [500, 600, 150, 800, 2500]))
