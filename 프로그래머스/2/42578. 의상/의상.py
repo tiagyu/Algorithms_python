6da060ae4a911ae55c8176302100ee92a8440756
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2024-10-27 19:39:14 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,18 @@
+def solution(clothes):
+    
+    # hash 생성
+    hashDict = {}
+    
+    for _, category in clothes:
+        if category in hashDict:
+            hashDict[category] += 1
+        else:
+            hashDict[category] = 1
+    
+    # 조합 구하기
+    answer = 1
+    
+    for _, value in hashDict.items():
+        answer *= (value + 1)
+        
+    return answer - 1
