9a3d69c41d8f256e612ffbff985c2eb2e9e9f8fd
tiagyu <tkroh1997@gmail.com>
2024-11-12 23:31:36 +0900

    Update 2161_카드1.py

--- 백준 코딩테스트/실버/2161_카드1.py
+++ 백준 코딩테스트/실버/2161_카드1.py
@@ -1,2 +1,20 @@
 # 2161 : 카드 1
+from collections import deque
+
 N = int(input())
+
+# 리스트 생성
+queue = deque(range(1,N+1))
+
+answer = []
+
+while len(queue) > 1:
+    # 맨 앞의 카드 버림
+    answer.append(queue.popleft())
+    # 그 다음 카드 맨 뒤로 이동
+    queue.append(queue.popleft())
+
+# 마지막 카드 반영
+answer.append(queue.popleft())
+
+print(answer)
