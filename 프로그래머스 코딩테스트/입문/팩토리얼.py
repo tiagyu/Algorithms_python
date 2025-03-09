c37f8f1834498ba3ebdfaf91903199ac2bfaa805
tiagyu <tkroh1997@gmail.com>
2025-03-09 12:41:08 +0900

    Update 팩토리얼.py

--- 프로그래머스 코딩테스트/입문/팩토리얼.py
+++ 프로그래머스 코딩테스트/입문/팩토리얼.py
@@ -8,3 +8,22 @@ def solution(n):
             answer = i-1
             break
     return answer
+
+# 다른 사람 풀이 1
+from math import factorial
+
+def solution2(n):
+    k = 10
+    while n < factorial(k):
+        k -= 1
+    return k
+
+# 다른 사람 풀이 2
+def solution3(n):
+    answer = 1
+    factorial = 1
+    while factorial <= n:
+        answer += 1
+        factorial *= answer
+    answer -= 1
+    return answer
