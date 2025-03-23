256d016348cd5c4b3bf5a8dc153987b04807695a
tiagyu <tkroh1997@gmail.com>
2025-03-24 01:55:34 +0900

    Update prac.py

--- 실무에 바로 쓰이는 알고리즘 by Python/2주차/prac.py
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/prac.py
@@ -1,3 +1,4 @@
+# 연결 리스트
 def isPalindrome(ln):
     arr = []
     head = ln.head
@@ -17,3 +18,25 @@ def isPalindrome(ln):
             return False
 
     return True
+
+# 스택
+def test_problem_stack(s):
+    pair = {
+        '}': '{',
+        ')': '(',
+        ']': '[',
+    }
+    opener = "({["
+    stack = []
+
+    for char in s:
+        if char in opener:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            top = stack.pop()
+            if pair[char] != top:
+                return False
+
+    return not stack
