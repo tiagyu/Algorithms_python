76c489337d174df440f47c9d945e707458284e85
tiagyu <tkroh1997@gmail.com>
2025-03-24 01:55:39 +0900

    Update structures.py

--- 실무에 바로 쓰이는 알고리즘 by Python/2주차/structures.py
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/structures.py
@@ -1,3 +1,4 @@
+# 연결 리스트
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -18,3 +19,29 @@ class LinkedList:
             node = node.next
 
         node.next = ListNode(val, None)
+
+# 스택
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        self.top = Node(value, self.top)
+
+    def pop(self):
+        if self.top is None:
+            return None
+
+        node = self.top
+        self.top = self.top.next
+
+        return node.val
+
+    def is_empty(self):
+        return self.top is None
