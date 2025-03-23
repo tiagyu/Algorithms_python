67a14b3c3da92bd6d66a10b7cb6f0163ece9e1cf
tiagyu <tkroh1997@gmail.com>
2025-03-24 01:55:44 +0900

    Update test.py

--- 실무에 바로 쓰이는 알고리즘 by Python/2주차/test.py
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/test.py
@@ -1,7 +1,7 @@
-from structures import LinkedList
-from prac import isPalindrome
+from structures import LinkedList, Stack
+from prac import isPalindrome, test_problem_stack
 
-
+# 연결 리스트
 l1 = LinkedList()
 for num in [1, 2, 2, 1]:
     l1.append(num)
@@ -13,3 +13,30 @@ for num in [1, 2]:
 
 assert isPalindrome(l1)
 assert not isPalindrome(l2)
+
+# 스택
+def test_stack():
+    stack = Stack()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    assert stack.pop() == 5
+    assert stack.pop() == 4
+    assert stack.pop() == 3
+    assert stack.pop() == 2
+    assert stack.pop() == 1
+    assert stack.pop() is None
+    assert stack.is_empty()
+    
+assert test_problem_stack("()")
+assert test_problem_stack("()[]{}")
+assert test_problem_stack("({[][]})")
+assert test_problem_stack("({[]})")
+assert not test_problem_stack("(]")
+assert not test_problem_stack("(()]")
+assert not test_problem_stack("(((])")
+assert not test_problem_stack("((())")
