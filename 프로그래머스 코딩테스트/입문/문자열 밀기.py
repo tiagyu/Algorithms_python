5a9ee0a72d9598cc01319204a5228c1f0ce84534
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-04-20 14:32:02 +0900

    Update 문자열 밀기.py

--- 프로그래머스 코딩테스트/입문/문자열 밀기.py
+++ 프로그래머스 코딩테스트/입문/문자열 밀기.py
@@ -1,4 +1,23 @@
 # 문자열 밀기
 def solution(A, B):
+    a_list = list(A)
     answer = 0
-    return answer
+
+    for _ in range(len(A)):
+        if "".join(a_list) == B:
+            return answer
+        last = a_list.pop()
+        a_list.insert(0, last)
+        answer += 1
+    return -1
+
+# 다른 사람 풀이1
+from collections import deque
+
+def solution(A, B):
+    a, b = deque(A), deque(B)
+    for cnt in range(0, len(A)):
+        if a == b:
+            return cnt
+        a.rotate(1)
+    return -1
