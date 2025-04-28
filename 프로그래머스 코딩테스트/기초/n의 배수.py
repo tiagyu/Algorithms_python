8e8ca0c616f6c7b9369fde4232eccef7409997af
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-04-28 11:07:32 +0900

    Update n의 배수.py

--- 프로그래머스 코딩테스트/기초/n의 배수.py
+++ 프로그래머스 코딩테스트/기초/n의 배수.py
@@ -3,3 +3,11 @@ def solution(num, n):
     if num % n == 0:
         return 1
     return 0
+
+# 다른 사람 풀이1
+def solution1(num, n):
+    return int(not(num % n))
+
+# 다른 사람 풀이2
+def solution2(num, n):
+    return int(num % n == 0)
