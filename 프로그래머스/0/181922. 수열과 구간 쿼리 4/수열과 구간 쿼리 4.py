21a793ed9e7a6c20c0f9ad3a6a862762ce11468a
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-05-20 13:10:59 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 10.96 ms, Memory: 9.53 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,9 @@
+def solution(arr, queries):
+    for s,e,k in queries:
+        for i in range(s,e+1):
+            if i:
+                if i % k == 0:
+                    arr[i] += 1
+            else:
+                arr[i] += 1
+    return arr
