2a502264b3e03d54fb3be28ac1f606ded1be431c
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-05-24 23:44:18 +0900

    Create 글자 이어 붙여 문자열 만들기.py

---
+++ 프로그래머스 코딩테스트/기초/글자 이어 붙여 문자열 만들기.py
@@ -0,0 +1,8 @@
+# 글자 이어 붙여 문자열 만들기
+def solution(my_string, index_list):
+    answer = ''
+    return answer
+
+my_string, index_list = "cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7]
+
+print(my_string.index('c'))
