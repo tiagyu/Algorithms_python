8d465e52ca9c40a5ce01224e832d47c8487f6ba3
tiagyu <tkroh1997@gmail.com>
2025-06-07 23:38:04 +0900

    Update 세로 읽기.py

--- 프로그래머스 코딩테스트/기초/세로 읽기.py
+++ 프로그래머스 코딩테스트/기초/세로 읽기.py
@@ -6,11 +6,12 @@ def solution(my_string, m, c):
     return "".join(answer)
 
 # 다른 사람 풀이1
-def solution(s, m, c):
+def solution1(s, m, c):
     return s[c-1::m]
 
 # 다른 사람 풀이2
-def solution(my_string, m, c):
+def solution2(my_string, m, c):
     answer = ''
-    for i in range(c-1,len(my_string),m): answer+=my_string[i]
+    for i in range(c-1,len(my_string),m): 
+        answer+=my_string[i]
     return answer
