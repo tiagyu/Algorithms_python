8f1d4ad9f68fb19a569ee9c24225c0bd3296a3fe
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-06-27 15:41:11 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- 프로그래머스 코딩테스트/모든 문제/Level 1/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스 코딩테스트/모든 문제/Level 1/x만큼 간격이 있는 n개의 숫자.py
@@ -1,7 +1,3 @@
 # x만큼 간격이 있는 n개의 숫자
 def solution(x, n):
-    if x > 0:
-        answer = [i for i in range(x,x*n+1,x)]
-    else:
-        answer = [i for i in range(x,x*n-1,x)]
-    return answer
+    return [x * i for i in range(1,n+1)]
