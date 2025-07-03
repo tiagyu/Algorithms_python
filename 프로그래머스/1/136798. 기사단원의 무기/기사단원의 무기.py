acf28d7429e508b2b8996cf98cadfbdd1f8524ff
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-07-03 12:55:55 +0900

    [level 1] Title: 기사단원의 무기, Time: 1852.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -2,13 +2,11 @@ def solution(number, limit, power):
     num_list = [1]
     for i in range(2,number+1):
         count = 0
-        j = 1
-        while j * j <= i:
-            if i % j == 0:
+        for j in range(1, int(i**0.5)+1):
+            if j * j == i:
                 count += 1
-                if j != i // j:
-                    count += 1
-            j += 1
+            elif i % j == 0:
+                count += 2
         if count > limit:
             count = power
         num_list.append(count)
