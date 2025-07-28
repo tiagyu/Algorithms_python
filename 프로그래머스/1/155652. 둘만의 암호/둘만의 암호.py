af8c5bab7758047344a84d63f27921b6c2425ece
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-07-28 14:29:31 +0900

    [level 1] Title: 둘만의 암호, Time: 0.04 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,16 @@
+def solution(s, skip, index):
+    answer = ''
+    alphabet = [chr(i) for i in range(ord('a'),ord('z')+1)]
+
+    for ch in skip:
+        if ch in alphabet:
+            alphabet.remove(ch)
+        
+    alphabet *= 2
+
+    for ch in s:
+        cur_idx = alphabet.index(ch)
+        new_idx = (cur_idx + index) % len(alphabet)
+        answer += alphabet[new_idx]
+
+    return answer
