4708d4ada0158593ad490fb4aa35abf80db31d30
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-08-07 01:13:28 +0900

    Update structures.py

--- 실무에 바로 쓰이는 알고리즘 by Python/2주차/structures.py
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/structures.py
@@ -45,3 +45,29 @@ class Stack:
 
     def is_empty(self):
         return self.top is None
+    
+# 큐
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def push(self, value):
+        if not self.front:
+            self.front = Node(value)
+            return
+
+        node = self.front
+        while node.next:
+            node = node.next
+        node.next = Node(value)
+
+    def pop(self):
+        if not self.front:
+            return None
+
+        node = self.front
+        self.front = self.front.next
+        return node.val
+
+    def is_empty(self):
+        return self.front is None
