5410f46f2866f5a8932a9327f47e2c2132880a2f
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-08-09 23:22:04 +0900

    Update test.py

--- 실무에 바로 쓰이는 알고리즘 by Python/2주차/test.py
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/test.py
@@ -1,5 +1,5 @@
-from structures import LinkedList, Stack
-from prac import isPalindrome, test_problem_stack
+from structures import LinkedList, Stack, Queue
+from prac import isPalindrome, test_problem_stack, test_problem_queue
 
 # 연결 리스트
 l1 = LinkedList()
@@ -40,3 +40,30 @@ assert not test_problem_stack("(]")
 assert not test_problem_stack("(()]")
 assert not test_problem_stack("(((])")
 assert not test_problem_stack("((())")
+
+# 큐
+def test_queue():
+    queue = Queue()
+
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    queue.push(4)
+    queue.push(5)
+
+    assert queue.pop() == 1
+    assert queue.pop() == 2
+    assert queue.pop() == 3
+    assert queue.pop() == 4
+    assert queue.pop() == 5
+    assert queue.pop() is None
+    assert queue.is_empty()
+    
+test_queue()
+
+assert test_problem_queue(2) == 2
+assert test_problem_queue(3) == 2
+assert test_problem_queue(4) == 4
+assert test_problem_queue(5) == 2
+assert test_problem_queue(6) == 4
+assert test_problem_queue(7) == 6
