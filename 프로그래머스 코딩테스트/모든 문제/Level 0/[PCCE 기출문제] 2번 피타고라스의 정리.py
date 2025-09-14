191850e8bb7629a985c428c09661d05e43629653
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2025-09-14 16:19:12 +0900

    Create [PCCE 기출문제] 2번 피타고라스의 정리.py

---
+++ 프로그래머스 코딩테스트/모든 문제/Level 0/[PCCE 기출문제] 2번 피타고라스의 정리.py
@@ -0,0 +1,13 @@
+# [PCCE 기출문제] 2번 / 피타고라스의 정리
+a = int(input())
+c = int(input())
+
+b_square = c**2 - a**2
+print(b_square)
+
+# 다른 사람 풀이1
+a = int(input())
+c = int(input())
+
+b_square = pow(c, 2) - pow(a, 2)
+print(b_square)
